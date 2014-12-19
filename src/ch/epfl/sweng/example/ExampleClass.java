1a31926991479d63a9e4a64868c888f18bf0e4b2
Blueur <blueur-ii@hotmail.com>
2014-12-19 11:03:50 +0100

    test

--- src/ch/epfl/sweng/example/ExampleClass.java
+++ src/ch/epfl/sweng/example/ExampleClass.java
@@ -2,14 +2,15 @@ package ch.epfl.sweng.example;
 
 /**
  * Sample class
+ * 
  * @author SwengStaff
- *
+ * 
  */
 public class ExampleClass {
-    public ExampleClass() {
-    }
-    
-    public int add(int a, int b) {
-        return a + b;
-    }
+	public ExampleClass() {
+	}
+
+	public int add(int a, int b) {
+		return a + b;
+	}
 }
